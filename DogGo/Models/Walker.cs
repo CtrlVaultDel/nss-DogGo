429354a0e9de6d40df26f90ae7d4c4327b7d0d9d
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-31 14:35:39 -0500

    Added attribute tags to Dogs and Walkers

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,14 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
+using System.ComponentModel;
 
 namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+        [DisplayName("Profile Picture")]
         public string ImageUrl { get; set; }
+        [DisplayName("Total Walk Time")]
         public string TotalWalkTime { get; set; }
         public Neighborhood Neighborhood { get; set; }
     }
